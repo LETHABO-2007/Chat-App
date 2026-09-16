69257ef9078a77191d9cfa04f23b7115bf28bd5e
LETHABO-2007 <lethabochidi17@gmail.com>
2026-09-16 23:10:17 +0200

    added files to github

---
+++ src/chatapp/ChatApp.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package chatapp;
+
+/**
+ *
+ * @author TECH 1
+ */
+ import java.util.Scanner;
+public class ChatApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("=== USER REGISTRATION ===");
+        System.out.print("Enter First Name: ");
+        String firstName = scanner.nextLine();
+    }
+    
+}
