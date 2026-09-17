516875ea74bd809f39d133cac625aac23972922a
LETHABO-2007 <lethabochidi17@gmail.com>
2026-09-17 09:16:30 +0200

    Update ChatApp.java

--- src/chatapp/ChatApp.java
+++ src/chatapp/ChatApp.java
@@ -161,4 +161,22 @@ public class Login {
             return "Username or password incorrect, please try again.";
         }
     }
+    
+    // Getters and Setters
+    public String getUsername() { return username; }
+    public void setUsername(String username) { this.username = username; }
+
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
+
+    public String getCellPhoneNumber() { return cellPhoneNumber; }
+    public void setCellPhoneNumber(String cellPhoneNumber) { this.cellPhoneNumber = cellPhoneNumber; }
+
+    public String getFirstName() { return firstName; }
+    public void setFirstName(String firstName) { this.firstName = firstName; }
+
+    public String getLastName() { return lastName; }
+    public void setLastName(String lastName) { this.lastName = lastName; }
 }
+
+
